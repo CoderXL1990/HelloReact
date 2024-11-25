98575021a199ea8ee4d6b7ac79aa375de316b9e0
Alan <coderxiaoyanglan@gmail.com>
2024-11-25 20:21:01 +0900

    ページを追加します

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Logo } from './components/Logo'
-import  Body  from './components/Body'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Logo } from "./components/Logo";
+import Body from "./components/Body";
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <Logo 
-        src = {viteLogo} 
-        url="https://vite.dev"
-        alt="Vite logo"
-        
-          />
-
-        <Logo 
-        src = {reactLogo} 
-        url="https://react.dev"
-        alt="React logo"
-        isReact
-          />
-
-      
+        <Logo src={viteLogo} url="https://vite.dev" alt="Vite logo" />
+        <Logo
+          src={reactLogo}
+          url="https://react.dev"
+          alt="React logo"
+          isReact
+        />
       </div>
-      <Body count = {count} onButtonClick= {() => setCount((count) => count + 1)}/>
-      
+
+      <Body
+        count={count}
+        onButtonClick={() => setCount((count) => count + 1)}
+      />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        {/* 
+  <Route element={<AuthLayout />}>
+    <Route path="login" element={<Login />} />
+    <Route path="register" element={<Register />} />
+  </Route> */}
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
