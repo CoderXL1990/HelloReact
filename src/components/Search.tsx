34114ec79600aba7eedad3a6907e0760b4a7367b
Alan <coderxiaoyanglan@gmail.com>
2024-11-25 22:41:57 +0900

    useMemo

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -9,7 +9,7 @@ type Props = {
 const Search = (props: Props) => {
   const { onChange } = props;
   const [value, setValue] = useState("");
-  const func = useMemo( () => {
+  const func = useMemo(() => {
    return (e: React.ChangeEvent<HTMLInputElement>) => {
       setValue(e.target.value);
     }
