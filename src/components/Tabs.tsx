3bae4d7ddaedc08a2b02bde844172fab7243aa3d
Alan <coderxiaoyanglan@gmail.com>
2024-11-25 21:40:19 +0900

    map用いたJSXのループ

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,44 +1,40 @@
 import React from "react";
 import { NavLink } from "react-router";
 
+const tabItems = [
+  {
+    to: "/",
+    title: "Home",
+  },
+  {
+    to: "/todo",
+    title: "Todo",
+  },
+];
+
 const Tabs = () => {
   return (
     <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
       <ul className="flex flex-wrap -mb-px">
-        <li className="me-2">
-          <NavLink
-            to="/"
-            className={({ isActive }) => {
-              const style = "inline-block p-4 border-b-2  rounded-t-lg";
+        {tabItems.map((item) => (
+          <li className="me-2" key={item.title}>
+            <NavLink
+              to={item.to}
+              className={({ isActive }) => {
+                const style = "inline-block p-4 border-b-2  rounded-t-lg";
 
-              const activeStyle = " text-blue-600 border-blue-600 active ";
+                const activeStyle = " text-red-600 border-red-600 active ";
 
-              return isActive
-                ? `${style} ${activeStyle}`
-                : `${style} border-transparent hover:text-gray-600 hover:border-gray-300`;
-            }}
-            end
-          >
-            Home kkk
-          </NavLink>
-        </li>
-        <li className="me-2">
-          <NavLink
-            to="/todo"
-            className={({ isActive }) => {
-              const style = "inline-block p-4 border-b-2  rounded-t-lg";
-
-              const activeStyle = " text-blue-600 border-blue-600 active ";
-
-              return isActive
-                ? `${style} ${activeStyle}`
-                : `${style} border-transparent hover:text-gray-600 hover:border-gray-300`;
-            }}
-            end
-          >
-            todo
-          </NavLink>
-        </li>
+                return isActive
+                  ? `${style} ${activeStyle}`
+                  : `${style} border-transparent hover:text-gray-600 hover:border-gray-300`;
+              }}
+              end
+            >
+              {item.title}
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </div>
   );
